f64d56fe49e8eba0c642116c5e49194c742a2783
Karl Trout <karl.trout@gmail.com>
2017-01-16 21:33:33 -0500

    added SRW logo and animations to menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+    trigger,
+    state,
+    style,
+    transition,
+    animate } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'menu-component',
   templateUrl: 'menu.component.html',
   styleUrls: ['menu.component.css'],
-
+  animations: [
+    trigger('shrinkUp', [
+      state('in', style({
+        backgroundColor: '#eee',
+        transform: 'translateY(0%)'
+      })),
+      state('out',   style({
+        backgroundColor: '#cfd8dc',
+        transform: 'translateY(-90%)'
+      })),
+      transition('in => out', animate('500ms ease-in')),
+      transition('out => in', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class MenuComponent  {
 
@@ -13,11 +31,29 @@ export class MenuComponent  {
   private clientX = 0;
   private clientY = 0;
 
+  private mouseEntered="out";
+
   private coordinates(event: MouseEvent):void {
     this.event = event;
     this.clientX = event.clientX;
     this.clientY = event.clientY;
     console.log("X "+this.clientX+" Y: "+this.clientY);
+  }
+
+  private onEvent(event: MouseEvent): void {
+
+    console.log("Event -> " + event.type);
+
+    if(event.type === "mouseleave"){
+       this.mouseEntered="out";
+      console.log("Event -> " + this.mouseEntered);
+    }
+
+    if(event.type === "mouseenter"){
+       this.mouseEntered="in";
+      console.log("Event -> " + this.mouseEntered);
+    }
+
   }
 
 }
