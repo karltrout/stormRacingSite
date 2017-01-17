47a4667cbb55c42502e60c5641f2ed633f000c9d
Karl Trout <karl.trout@gmail.com>
2017-01-16 21:48:02 -0500

    added signup component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuDirective } from './menu/menu.directive';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    MenuDirective ],
+    MenuDirective,
+    SignupComponent ],
   imports: [
     BrowserModule,
     FormsModule,
